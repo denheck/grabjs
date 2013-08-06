cf001ca03a919a589ca81fcbd338a35e68df0973
Dennis Heckman <dennis.heckman@vertive.com>
2013-08-05 20:26:41 -0500

    initial grab.js script

---
+++ grab.js
@@ -0,0 +1,29 @@
+;(function ($) {
+  // load file
+  var load = function (filename) {
+    return $.ajax({
+      url: pathToScript,
+      dataType: 'script'
+    });
+  };
+
+  // functions for loading different filetypes
+  var filetypeFunctions = {
+    'js': load,
+    'css': function (pathToStyle) {
+      return load(pathToStyle).done(function (styles) {
+        $('head').append('<style>' + styles + '</style>');
+      });
+    }
+  };
+
+  // get file extension from filename
+  var getFileExtension = function (filename) {
+    return filename.join('.').pop();
+  };
+
+  // add grap plugin to jQuery
+  $.fn.grab = function (filename) {
+    return filetypeFunctions[getFileExtension(filename)]();
+  };
+})(jQuery);
